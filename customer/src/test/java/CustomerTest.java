ef031fbe0e1af4d94bdfc2036d788e30ca412a2f
ashgarg16 <43719843+ashgarg16@users.noreply.github.com>
2022-07-25 16:37:00 +0530

    RestAssured

---
+++ customer/src/test/java/CustomerTest.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+
+public class CustomerTest {
+
+    public static void main(String[] args) {
+
+        RestAssured.baseURI="https://localhost:8081";
+//        given().queryParam()
+
+
+
+    }
+}
